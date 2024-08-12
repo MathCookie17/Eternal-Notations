c0103d02acec8e259d8463bbba4e6ca92fcb8373
ghoqyew <ghoqyew@proton.me>
2024-08-12 16:27:02 +0800

    Hypersplit Base 2

--- src/presets.ts
+++ src/presets.ts
@@ -526,6 +526,8 @@ PresetAssembly.Hypersplit = new HypersplitNotation(...[,,,,,,,], defaultRound).s
 HTMLPresetAssembly.Hypersplit = new HypersplitNotation(...[,,,,,,,], defaultRound).setName("Hypersplit");
 PresetAssembly.HypersplitBase3 = new HypersplitNotation([["", ""], ["*3^", ""], ["((3^)^", ") "], ["((3^^)^", ") "]], 3, [3, 3, 3], ...[,,,,], defaultRound).setName("Hypersplit (Base 3)");
 HTMLPresetAssembly.HypersplitBase3 = new HypersplitNotation([["", ""], ["*3^", ""], ["((3^)^", ") "], ["((3^^)^", ") "]], 3, [3, 3, 3], ...[,,,,], defaultRound).setName("Hypersplit (Base 3)");
+PresetAssembly.HypersplitBase2 = new HypersplitNotation([["", ""], ["*2^", ""], ["((2^)^", ") "], ["((2^^)^", ") "]], 2, [4, 4, 4], ...[,,,,], defaultRound).setName("Hypersplit (Base 2)");
+HTMLPresetAssembly.HypersplitBase2 = new HypersplitNotation([["", ""], ["*2^", ""], ["((2^)^", ") "], ["((2^^)^", ") "]], 2, [4, 4, 4], ...[,,,,], defaultRound).setName("Hypersplit (Base 2)");
 
 PresetAssembly.HyperE = new ConditionalNotation(false,
     [
@@ -1110,6 +1112,7 @@ let Presets = {
     DoubleBinaryPrefixes: PresetAssembly.DoubleBinaryPrefixes,
     Hypersplit: PresetAssembly.Hypersplit,
     HypersplitBase3: PresetAssembly.HypersplitBase3,
+    HypersplitBase2: PresetAssembly.HypersplitBase2,
     HyperE: PresetAssembly.HyperE,
     Infinity: PresetAssembly.Infinity,
     Eternity: PresetAssembly.Eternity,
@@ -1236,6 +1239,7 @@ let HTMLPresets = {
     DoubleBinaryPrefixes: HTMLPresetAssembly.DoubleBinaryPrefixes,
     Hypersplit: HTMLPresetAssembly.Hypersplit,
     HypersplitBase3: HTMLPresetAssembly.HypersplitBase3,
+    HypersplitBase2: HTMLPresetAssembly.HypersplitBase2,
     HyperE: HTMLPresetAssembly.HyperE,
     Infinity: HTMLPresetAssembly.Infinity,
     Eternity: HTMLPresetAssembly.Eternity,
